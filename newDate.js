4bdc5a1a2c218794fb9450ec558dbb1365c06846
Roko C. Buljan <roko.cb@gmail.com>
2016-10-11 22:23:55 +0200

    Update newDate.js
    polishing property names

--- newDate.js
+++ newDate.js
@@ -1,22 +1,22 @@
 /**
 * newDate() - Get a new Date by passing extra parameters - by roko.cb
-* @param {(object|number|string)} date - Date Object, String or timestamp number
-* @param {string} adjust - Adds or subtracts. "+7d" or like "-1y,+2m,+14d"
-* @param {string} nearest - To nearest weekday "0"(Sun) "6"(Sat). "-0" for previous Sunday
-* @return {object} - Returns a new Date Object
+* @param {(string|number|object)} date - String, timestamp or Date Object
+* @param {string} modifiers - Adds or subtracts. "+7d" or like "-1y,+2m,+14d"
+* @param {string} weekday - To nearest weekday "0"(Sun) "6"(Sat). "-0" for previous Sunday
+* @return {object} - Returns a new Date Object with the reslut.
 */
-function newDate(date, adjust, nearest){
+function newDate(date, modifiers, weekday){
   if(!date){
     date = new Date();
   }else if(typeof date !== "object"){
     date = new Date(date);
   }
-  if(adjust && typeof adjust === "string") {
-    adjust = adjust.split(",");
-    while(adjust.length){
-      var a = adjust.shift(),
-          num = a.split(/[dmy]/i)[0],
-          dmy = a.replace(num,"")||"d",
+  if(modifiers && typeof modifiers === "string") {
+    modifiers = modifiers.split(",");
+    while(modifiers.length){
+      var mod = modifiers.shift(),
+          num = mod.split(/[dmy]/i)[0],
+          dmy = mod.replace(num,"")||"d",
           props = {
             y : ["setFullYear","getFullYear"],
             m : ["setMonth","getMonth"],
@@ -25,9 +25,9 @@ function newDate(date, adjust, nearest){
       date = new Date(date[props[0]](date[props[1]]()+(+num)));
     }
   }
-  if(nearest && typeof nearest === "string"){
-    var i = nearest.indexOf("-")>-1?-1:1;
-    while (date.getDay() !== Math.abs(+nearest%7)) {
+  if(weekday && typeof weekday === "string"){
+    var i = weekday.indexOf("-")>-1?-1:1;
+    while (date.getDay() !== Math.abs(+weekday%7)) {
       date.setDate(date.getDate()+i);
     }
   }
